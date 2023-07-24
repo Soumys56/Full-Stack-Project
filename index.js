76b7ebb612e1ee0312428fc68fe4ff8f18708336
Soumys56 <soumyabera56@gmail.com>
2023-07-24 23:59:42 +0530

    extract the style and script from subpages into the layout

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 
-app.use(expressLayouts)
+//use static file 
+app.use(express.static('./assests'));
+app.use(expressLayouts);
+
+//extract the style and script from the subpages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 //use express router.this is a used as a middleware
 app.use('/',require('./router'));
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err)
