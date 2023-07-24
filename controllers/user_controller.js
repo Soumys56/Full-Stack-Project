35804d69dbc5a3a1ec55fa4b97bb297fa24d512f
Soumys56 <soumyabera56@gmail.com>
2023-07-25 03:02:19 +0530

    here i am creating my signup page and creating a route for this

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1> user profile</h1>');
+    return res.render('profile');
 }
+module.exports.signUp=function (req,res) {
+    return res.render('sign_up');
+  }
--- router/fbuser.js
+++ router/fbuser.js
@@ -5,6 +5,7 @@ const router=express.Router();
 const userController=require('../controllers/user_controller');
 
 router.get('/profile',userController.profile);
+router.get('/sign_up',userController.signUp);
 
 
 module.exports=router;
