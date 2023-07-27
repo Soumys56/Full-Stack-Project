a53936bb055452229f809f8294ed12fe3f0f2673
Soumys56 <soumyabera56@gmail.com>
2023-07-27 23:40:31 +0530

    crete my instragram post page navbar

--- assests/js/profile.js
+++ assests/js/profile.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+// const search=document.querySelectorAll("img")[1];
+// search.addEventListener('click',()=>{
+    
+    
+// })
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 7,
+    spaceBetween: 5,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+      clickable: true,
+      
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
